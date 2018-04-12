269ef27da5d643df6ead4602d4e36756329a47bf
JPMOS <mcguireenhs@gmail.com>
2018-04-12 09:13:17 -0500

    first

---
+++ bikeshare.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import seaborn as sns
+import scikitlearn 
+
+train = pd.read_csv("bikeshareTRAIN.csv")
+test = pd.read_csv("bikeshareTEST.csv")
+
+print(train.describe())
+
+train.head(3)
+train.isnull().sum()
+
+# 1. combine dataframes for modeling later. 
+bikes = pd.concat([train, test])
+
+# 2. separate numerical from categorical.
+
+bike.astype()
+print(bikes[0:4,0:4])
+
+
+sns.pairplot(train)
+plt.show()
+
+pd.isna
+??pandas.isna
+
+A = np.random.rand(3,3)
+eigenvals, eigenvecs = np.linalg.eig(A)
