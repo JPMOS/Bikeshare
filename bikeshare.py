fb7da6c022f2f0255c39781e5e26d0adb7e33011
kschiff17 <38381440+kschiff17@users.noreply.github.com>
2018-04-19 19:05:39 -0500

    Update bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -7,20 +7,39 @@ import scikitlearn
 train = pd.read_csv("bikeshareTRAIN.csv")
 test = pd.read_csv("bikeshareTEST.csv")
 
+print(train.shape)
+print(test.shape)
+
 print(train.describe())
 
 train.head(3)
 train.isnull().sum()
+
+# Separate dates into date and hour
+date = pd.DatetimeIndex(train['datetime'])
+train['date'] = date.date
+train['hour'] = date.time
+del train['datetime']
+train.head()
+
+# Separate dates into date and hour
+date = pd.DatetimeIndex(test['datetime'])
+test['date'] = date.date
+test['hour'] = date.time
+del test['datetime']
+test.head()
 
 # 1. combine dataframes for modeling later. 
 bikes = pd.concat([train, test])
 
 # 2. separate numerical from categorical.
 
-bike.astype()
+bikes.astype()
 print(bikes[0:4,0:4])
 
 
 sns.pairplot(train)
 plt.show()
 
+# correlation heatmap
+sns.clustermap(train.corr(), center=0, linewidths=.5, figsize=(13, 13), vmax=.5, square=True,annot=True)
